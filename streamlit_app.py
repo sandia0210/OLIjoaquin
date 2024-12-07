573533582e957efee044cc3ff87fa5b42a65e19d
MarisolVizu <166885795+MarisolVizu@users.noreply.github.com>
2024-12-07 17:53:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,22 +16,19 @@ def load_data():
     url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=xlsx"
     df = pd.read_excel(url, sheet_name=sheet_name, engine='openpyxl')
 
-
     # Filtro por iniciativas activas
     df = df[df['¿Su iniciativa se encuentra activa?'].str.lower() != 'no']
 
-    # cambiar nombres
-
-    df['Objetivo_General']=df['En 50 palabras o menos, por favor, describe el objetivo general de la iniciativa/programa']
-    df['Responsable'] =  df['Nombre del Responsable'] + ' - ' + df['Cargo del Responsable']
-    df['Contacto'] =  df['Número de Contacto (Ejemplo: (+51) 949972341)']
-    df['Facebook'] =df['Facebook de la organización (link)']
-    df['Instagram']=df['Instagram de la organización (link)']
-    df['Página_web']=df['Página web de la organización (link)']
+    # Cambiar nombres
+    df['Objetivo_General'] = df['En 50 palabras o menos, por favor, describe el objetivo general de la iniciativa/programa']
+    df['Responsable'] = df['Nombre del Responsable'] + ' - ' + df['Cargo del Responsable']
+    df['Contacto'] = df['Número de Contacto (Ejemplo: (+51) 949972341)']
+    df['Facebook'] = df['Facebook de la organización (link)']
+    df['Instagram'] = df['Instagram de la organización (link)']
+    df['Página_web'] = df['Página web de la organización (link)']
 
     # Limpieza de texto relevante
     df['COMUNIDAD_CLEAN'] = df['Tipo de comunidad/población más beneficiado por las actividades de la organización']
-
 
     # Crear lista de departamentos con manejo de nulos
     df['Departamentos_lista'] = df.apply(
@@ -41,7 +38,6 @@ def load_data():
         if pd.notna(row['¿En qué parte del Perú opera la organización?']) else [],
         axis=1
     )
-    #df['Departamentos_lista'] = df['Departamentos_lista'].apply(lambda x: [clean_text(dep) for dep in x])  # Limpiar texto en la lista
 
     # Combinar prioridades en una sola columna
     prioridad_cols = [
@@ -53,9 +49,9 @@ def load_data():
     ]
     df['PRIORIDADES'] = (
         df[prioridad_cols]
-        .apply(lambda row: ' '.join(row.dropna().astype(str)), axis=1)  # Ignorar nulos
-        .str.replace(r'\s+', ' ', regex=True)  # Limpiar espacios extra
-        .str.strip()  # Eliminar espacios al inicio y final
+        .apply(lambda row: ' '.join(row.dropna().astype(str)), axis=1)
+        .str.replace(r'\s+', ' ', regex=True)
+        .str.strip()
     )
 
     # Otras columnas combinadas
@@ -83,26 +79,20 @@ def load_model():
 
 # Función para filtrar los datos según los criterios seleccionados
 def filter_data(df, comunidades_seleccionadas, departamentos_seleccionados):
-    # Filtrar por tipo de población beneficiaria
     if comunidades_seleccionadas:
         pattern = '|'.join([re.escape(c) for c in comunidades_seleccionadas])
         df = df[df['COMUNIDAD_CLEAN'].str.contains(pattern, case=False, na=False)]
-
-    # Filtrar por departamentos seleccionados
     if departamentos_seleccionados:
-        #departamentos_seleccionados_clean = [clean_text(dep) for dep in departamentos_seleccionados]
         df = df[df['Departamentos_lista'].apply(lambda x: any(dep in x for dep in departamentos_seleccionados))]
-
     return df
 
 # Función para encontrar ONGs similares
 def find_similar_ongs(description, embeddings, ong_names, combined_texts, model, top_n=5):
-    if len(embeddings) == 0:  # Check if embeddings are empty
+    if len(embeddings) == 0:
         return []
-
     description_embedding = model.encode(description, convert_to_tensor=True)
     cosine_scores = util.pytorch_cos_sim(description_embedding, embeddings)[0]
-    top_results = torch.topk(cosine_scores, k=min(top_n, len(ong_names)))  # Ensure top_n does not exceed available data
+    top_results = torch.topk(cosine_scores, k=min(top_n, len(ong_names)))
     similar_ongs = []
     for score, idx in zip(top_results.values, top_results.indices):
         similar_ongs.append({
@@ -113,69 +103,127 @@ def find_similar_ongs(description, embeddings, ong_names, combined_texts, model,
 
 # Interfaz principal
 def main():
-    st.title("🌟 Buscador de ONGs Relevantes")
-    st.markdown("Encuentra las organizaciones más relevantes según tus necesidades.")
 
-    # Botón para actualizar la base de datos
+    # Estilo general
+    st.markdown(
+        """
+        <style>
+            .stButton>button {
+                background-color: #E55200; 
+                color: white; 
+                border-radius: 5px; 
+                padding: 10px 20px;
+            }
+            .stDownloadButton>button {
+                background-color: #E55200;
+                color: white;
+                border-radius: 5px;
+                padding: 10px 20px;
+            }
+            .stMarkdown h1 {
+                color: #E55200;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    # Estilos CSS personalizados para el multiselect
+    custom_css = """
+    <style>
+        .stMultiSelect [role="option"] {
+            color: white; /* Color de texto de las opciones */
+            font-weight: bold; /* Negrita para destacar */
+        }
+        .stMultiSelect .st-af { /* Opciones seleccionadas */
+            background-color: #E55200; /* Fondo azul claro */
+            color: white; /* Texto azul oscuro */
+        }
+    </style>
+    """
+    # Renderizamos los estilos en la app
+    st.markdown(custom_css, unsafe_allow_html=True)
+
+
+    # CSS personalizado para cambiar el color del sidebar
+    sidebar_style = """
+    <style>
+        [data-testid="stSidebar"] {
+            background-color: #A8B8C7; /* Fondo azul calmado */
+            color: #002857; /* Texto en azul oscuro */
+        }
+        [data-testid="stSidebar"] .css-1d391kg {
+            color: #002857; /* Color de texto en los inputs */
+        }
+        [data-testid="stSidebar"] .css-17eq0hr {
+            color: #E55200; /* Color de los títulos al naranja vibrante */
+        }
+    </style>
+    """
+
+    # Aplicar los estilos en la app
+    st.markdown(sidebar_style, unsafe_allow_html=True)
+    
+    st.markdown("<h1 style='color: #001d57';'>Buscador de ONGs Relevantes</h1>", unsafe_allow_html=True)
+    st.markdown("<p style='color: #001d57';'>Encuentra las organizaciones más relevantes según tus necesidades.</p>", unsafe_allow_html=True)
+
+    
     with st.sidebar:
-        st.header("🔄 Configuración")
+        st.image("Assets/LOGO_Oli.png", use_container_width=True)
+        st.markdown("<h2 style='color: #001d57';'>↻ Configuración</h2>", unsafe_allow_html=True)
         if st.button("Actualizar Base de Datos"):
             st.cache_data.clear()
             st.success("Base de datos actualizada correctamente.")
 
-    # Cargar los datos y el modelo
     df = load_data()
     model = load_model()
 
-    # Opciones del filtro "Población Beneficiaria"
-    opciones_poblacion = [
-        "Comunidades por condiciones económicas y de acceso a servicios (como servicios tomar en cuenta: educación, agua, alimentos, etc.)",
-        "Comunidades por ubicación geográfica y cultural (población afrodescendiente, migrante o en zona de invasión)",
-        "Grupos por edad (niños o adultos mayores en situación de vulnerabilidad)",
-        "Grupos por genero (madres o mujeres desprotegidas, miembros de la comunidad LGBT+, etc.)",
-        "Personas con discapacidades (física, sensorial, neurológica, etc.)",
-        "Animales (en abandono o víctimas de tráfico ilegal)"
-    ]
-    #opciones_poblacion = [clean_text(i) for i in opciones_poblacion]
+    st.markdown("<h3 style='color: #001d57;'>↻ Filtros Iniciales</h3>", unsafe_allow_html=True)
+    comunidades_seleccionadas = st.multiselect(
+        "Seleccione el tipo de población beneficiaria:",
+        options=[
+            "Comunidades por condiciones económicas y de acceso a servicios (como servicios tomar en cuenta: educación, agua, alimentos, etc.)",
+            "Comunidades por ubicación geográfica y cultural (población afrodescendiente, migrante o en zona de invasión)",
+            "Grupos por edad (niños o adultos mayores en situación de vulnerabilidad)",
+            "Grupos por genero (madres o mujeres desprotegidas, miembros de la comunidad LGBT+, etc.)",
+            "Personas con discapacidades (física, sensorial, neurológica, etc.)",
+            "Animales (en abandono o víctimas de tráfico ilegal)"
+        ],
+        placeholder='Selecciona una o varias opciones'
+    )
+    departamentos_seleccionados = st.multiselect(
+        "Seleccione el departamento de operación:",
+        options=[
+            "Amazonas", "Áncash", "Apurimac", "Arequipa", "Ayacucho", "Cajamarca", "Callao",
+            "Cusco", "Huancavelica", "Huánuco", "Ica", "Junín", "La Libertad", "Lambayeque",
+            "Lima", "Loreto", "Madre de Dios", "Moquegua", "Pasco", "Piura", "Puno",
+            "San Martín", "Tacna", "Tumbes", "Ucayali"
+        ],
+        placeholder='Selecciona una o varias opciones'
+    )
 
-    # Opciones del filtro "Departamentos"
-    departamentos = [
-        "Amazonas", "Áncash", "Apurimac", "Arequipa", "Ayacucho", "Cajamarca", "Callao",
-        "Cusco", "Huancavelica", "Huánuco", "Ica", "Junín", "La Libertad", "Lambayeque",
-        "Lima", "Loreto", "Madre de Dios", "Moquegua", "Pasco", "Piura", "Puno",
-        "San Martín", "Tacna", "Tumbes", "Ucayali"
-    ]
-
-    # Mostrar filtros
-    st.subheader("🔄 Filtros Iniciales")
-    comunidades_seleccionadas = st.multiselect("Seleccione el tipo de población beneficiaria:", options=opciones_poblacion, placeholder='Selecciona una o varias opciones')
-    departamentos_seleccionados = st.multiselect("Seleccione el departamento de operación:", options=departamentos,placeholder='Selecciona una o varias opciones')
-
-    # Aplicar filtros
     df_filtrado = filter_data(df, comunidades_seleccionadas, departamentos_seleccionados)
 
     if df_filtrado.empty:
         st.error("No results match your filter criteria. Please adjust the filters.")
         return
 
-    # Preparar embeddings
     combined_texts = df_filtrado['COMBINED_TEXT'].tolist()
     ong_names = df_filtrado['Nombre de la iniciativa/Organización'].tolist()
     embeddings = model.encode(combined_texts, convert_to_tensor=True)
 
-    # Entrada del usuario
-    st.subheader("🔍 Buscar ONGs")
+    st.markdown("<h3 style='color: #001d57;'>🔍 Buscar ONGs</h3>", unsafe_allow_html=True)
     description = st.text_area("Describe lo que buscas:", placeholder="ONG que trabaje con niños con cancer.")
 
-    # Selección del Top N
-    top_n = st.number_input("Selecciona cuántas ONGs relevantes deseas (Top N):", min_value=1, max_value=50, value=5, step=1)
+    top_n = st.number_input(
+        "Selecciona cuántas ONGs relevantes deseas (Top N):", 
+        min_value=1, max_value=50, value=5, step=1
+    )
 
-    # Buscar ONGs relevantes
     if st.button("Buscar ONGs"):
         if description.strip() != "":
             similar_ongs = find_similar_ongs(description, embeddings, ong_names, combined_texts, model, top_n=top_n)
 
-            # Mostrar resultados más relevantes
             if not similar_ongs:
                 st.warning("No similar ONGs found. Try refining your description or adjusting filters.")
             else:
@@ -194,34 +242,32 @@ def main():
                     Página_web = ong_data['Página_web']
                     Departamentos = ', '.join(ong_data['Departamentos_lista'])
 
-                    # Generar tarjeta solo con valores no NaN
                     card_content = f"""
-                        <div style="background-color:#f9f9f9;padding:10px;margin-bottom:10px;border-radius:5px;border:1px solid #ddd;">
-                            <strong>🏢 ONG:</strong> {ong['ONG']}<br>
-                            <strong>📊 Puntaje:</strong> {ong['Score']:.4f}<br>
+                    <div style="background-color:#f9f9f9;padding:10px;margin-bottom:10px;border-radius:5px;border:1px solid #ddd; border-left: 5px solid transparent; background-image: linear-gradient(to right, #001d57 5px, #f9f9f9 5px);">
+                        <strong style='color:#001d57;'>🏢 ONG:</strong> {ong['ONG']}<br>
+                        <strong style='color:#001d57;'>📊 Puntaje:</strong> {ong['Score']:.4f}<br>
                     """
                     if pd.notna(comunidad):
-                        card_content += f"<strong>🌍 Comunidad:</strong> {comunidad}<br>"
+                        card_content += f"<strong style='color:#001d57;'>🌍 Comunidad:</strong> {comunidad}<br>"
                     if pd.notna(Departamentos):
-                        card_content += f"<strong>🗺️ Departamentos:</strong> {Departamentos}<br>"
+                        card_content += f"<strong style='color:#001d57;'>🗺️ Departamentos:</strong> {Departamentos}<br>"
                     if pd.notna(Objetivo_General):
-                        card_content += f"<strong>🎯 Objetivo General:</strong> {Objetivo_General}<br>"
+                        card_content += f"<strong style='color:#001d57;'>🎯 Objetivo General:</strong> {Objetivo_General}<br>"
                     if pd.notna(Responsable):
-                        card_content += f"<strong>👤 Responsable:</strong> {Responsable}<br>"
+                        card_content += f"<strong style='color:#001d57;'>👤 Responsable:</strong> {Responsable}<br>"
                     if pd.notna(Contacto):
-                        card_content += f"<strong>📞 Contacto:</strong> {Contacto}<br>"
+                        card_content += f"<strong style='color:#001d57;'>📞 Contacto:</strong> {Contacto}<br>"
                     if pd.notna(Facebook):
-                        card_content += f'<strong>📘 Facebook:</strong> <a href="{Facebook}" target="_blank">{Facebook}</a><br>'
+                        card_content += f'<strong style="color:#001d57;">📘 Facebook:</strong> <a href="{Facebook}" target="_blank">{Facebook}</a><br>'
                     if pd.notna(Instagram):
-                        card_content += f'<strong>📷 Instagram:</strong> <a href="{Instagram}" target="_blank">{Instagram}</a><br>'
+                        card_content += f'<strong style="color:#001d57;">📷 Instagram:</strong> <a href="{Instagram}" target="_blank">{Instagram}</a><br>'
                     if pd.notna(Página_web):
-                        card_content += f'<strong>🌐 Página web:</strong> <a href="{Página_web}" target="_blank">{Página_web}</a><br>'
+                        card_content += f'<strong style="color:#001d57;">🌐 Página web:</strong> <a href="{Página_web}" target="_blank">{Página_web}</a><br>'
 
                     card_content += "</div>"
 
                     st.markdown(card_content, unsafe_allow_html=True)
 
-                    # Agregar a resultados para exportar
                     ong['Comunidad'] = comunidad
                     ong['Departamentos'] = Departamentos
                     ong['Objetivo General'] = Objetivo_General
@@ -232,8 +278,6 @@ def main():
                     ong['Página web'] = Página_web
                     results.append(ong)
 
-
-                # Exportar resultados a Excel
                 df_results = pd.DataFrame(results)
                 output = io.BytesIO()
                 with pd.ExcelWriter(output, engine='openpyxl') as writer:
